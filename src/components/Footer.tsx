e6d62f2958a10361e6c4376ccc9de01c73f4c95c
Ian Vieira <ianmarques619@gmail.com>
2024-03-18 21:49:41 -0300

    adicionando a parte do footer na página

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import './Footer.sass'
+import { TiSocialLinkedin } from "react-icons/ti";
+import { CiInstagram } from "react-icons/ci";
+import { FaTwitch } from "react-icons/fa";
+import logoianvi from "../../public/LogoDark.png";
 
 const Footer = () => {
   return (
-    <div>
-      oi
-    </div>
+    <footer className='FooterPage'>
+      <div className='FooterArea'>
+        <p>
+          Ian Vieira © 2024. Todos os direitos reservados.
+        </p>
+        <ul>
+          <li className='InstagramIcon'><a href="https://www.instagram.com/ianvmc1/"><CiInstagram size={28}/></a></li>
+          <li className='TwitchIcon'><a href="https://www.twitch.tv/ianvi_"><FaTwitch  size={28}/></a></li>
+          <li className='LinkedinIcon'><a href="https://www.linkedin.com/in/ianvi/"><TiSocialLinkedin size={28}/></a></li>
+        </ul>
+        <img src={logoianvi} alt="Logo" />
+      </div>
+    </footer>
   )
 }
 
