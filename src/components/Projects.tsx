b07347c38a22c5e3a3340188d4a7fea71873c931
Ian Vieira <ianmarques619@gmail.com>
2024-03-21 17:04:13 -0300

    trocando imagens dos projects e ajustando o gap entre o footer e o project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,15 +10,15 @@ interface Project {
   text: string;
   img: JSX.Element;
   techs: string;
-}
+} 
 
 const projectsTech: Project[] = [
-  { id: "project1", name: "Project Awax", text: "Projeto criado em HTML e CSS pelo curso B7Web, landing page.", img: <img className='TechImage' src='../../public/projectsPortfolio.png' />, techs: "Html e Css."},
-  { id: "project2", name: "Project Starbucks", text: "Projeto criado em HTML e CSS pelo curso B7Web, landing page responsivo, clone do starbucks.", img: <img className='TechImage' src='../../public/projectsPortfolio2.png' />,techs: "Html e Css."},
-  { id: "project3", name: "Project Costs", text: "Projeto em React com api criada, pelo curso React do 'Matheus Battisti'.", img: <img className='TechImage' src='../../public/projectsPortfolio3.png' />,techs: "React."},
-  { id: "project4", name: "Project MultiStep", text: "Projeto em React usando um formulário, pelo curso React do 'Matheus Battisti'.", img: <img className='TechImage' src='../../public/projectsPortfolio4.png'/>,techs: "React."},
-  { id: "project5", name: "Project ModalPizza", text: "Projeto em JavaScript com api criada, modais com pizza, pelo curso B7Web.", img: <img className='TechImage' src='../../public/projectsPortfolio5.png' />,techs: "Html, Css e JavaScript."},  
-  { id: "project6", name: "Project GitHubSearch", text: "Projeto em React, api github, com Typescript, pelo curso React do 'Matheus Battisti'.", img: <img className='TechImage' src='../../public/projectsPortfolio7.png' />,techs: "React e TypeScript."}]
+  { id: "project1", name: "Project Awax", text: "Projeto criado em HTML e CSS pelo curso B7Web, landing page.", img: <img className='TechImage' src='../../public/imageport1.png' />, techs: "Html e Css."},
+  { id: "project2", name: "Project Starbucks", text: "Projeto criado em HTML e CSS pelo curso B7Web, landing page responsivo, clone do starbucks.", img: <img className='TechImage' src='../../public/imageport2.png' />,techs: "Html e Css."},
+  { id: "project3", name: "Project Costs", text: "Projeto em React com api criada, pelo curso React do 'Matheus Battisti'.", img: <img className='TechImage' src='../../public/imageport6.png' />,techs: "React."},
+  { id: "project4", name: "Project MultiStep", text: "Projeto em React usando um formulário, pelo curso React do 'Matheus Battisti'.", img: <img className='TechImage' src='../../public/imageport5.png'/>,techs: "React."},
+  { id: "project5", name: "Project ModalPizza", text: "Projeto em JavaScript com api criada, modais com pizza, pelo curso B7Web.", img: <img className='TechImage' src='../../public/imageport3.png' />,techs: "Html, Css e JavaScript."},  
+  { id: "project6", name: "Project GitHub Search", text: "Projeto em React, api github, com Typescript, pelo curso React do 'Matheus Battisti'.", img: <img className='TechImage' src='../../public/imageport4.png' />,techs: "React e TypeScript."}]
 
   const ProjectsPage: React.FC = () => {
     return (
