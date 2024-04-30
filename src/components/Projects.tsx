4054185229614dcaf3649e51860b730312be2a4d
Ian Vieira <ianmarques619@gmail.com>
2024-04-30 18:05:44 -0300

    adicionando os deploys do projeto no botão visualizar nos maps

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,14 +10,21 @@ interface Project {
   text: string;
   img: JSX.Element;
   techs: string;
+  deploy: JSX.Element;
 } 
 
 const projectsTech: Project[] = [
   { id: "project1", name: "Project Awax", text: "Projeto criado em HTML e CSS pelo curso B7Web, landing page.", img: <img className='TechImage' src='../../public/imageport1.png' />, techs: "Html e Css."},
   { id: "project2", name: "Project Starbucks", text: "Projeto criado em HTML e CSS pelo curso B7Web, landing page responsivo, clone do starbucks.", img: <img className='TechImage' src='../../public/imageport2.png' />,techs: "Html e Css."},
-  { id: "project3", name: "Project Costs", text: "Projeto em React com api criada, pelo curso React do 'Matheus Battisti'.", img: <img className='TechImage' src='../../public/imageport6.png' />,techs: "React."},
+  { id: "project3", name: "Project Costs", text: "Projeto em React com api criada, pelo curso React do 'Matheus Battisti'.", img: <img className='TechImage' src='../../public/imageport6.png' />,techs: "React.", deploy:<a href='https://costs-react-eight.vercel.app/' target='_blank' rel='noreferrer'>
+  <FaEye size={28} className='EyeIcon' />
+  <p>Visualizar</p>
+</a>},
   { id: "project4", name: "Project MultiStep", text: "Projeto em React usando um formulário, pelo curso React do 'Matheus Battisti'.", img: <img className='TechImage' src='../../public/imageport5.png'/>,techs: "React."},
-  { id: "project5", name: "Project ModalPizza", text: "Projeto em JavaScript com api criada, modais com pizza, pelo curso B7Web.", img: <img className='TechImage' src='../../public/imageport3.png' />,techs: "Html, Css e JavaScript."},  
+  { id: "project5", name: "Project ModalPizza", text: "Projeto em JavaScript com api criada, modais com pizza, pelo curso B7Web.", img: <img className='TechImage' src='../../public/imageport3.png' />,techs: "Html, Css e JavaScript.", deploy:<a href='https://ianvi1.github.io/modalPizza/' target='_blank' rel='noreferrer'>
+  <FaEye size={28} className='EyeIcon' />
+  <p>Visualizar</p>
+</a>}, 
   { id: "project6", name: "Project GitHub Search", text: "Projeto em React, api github, com Typescript, pelo curso React do 'Matheus Battisti'.", img: <img className='TechImage' src='../../public/imageport4.png' />,techs: "React e TypeScript."}]
 
   const ProjectsPage: React.FC = () => {
@@ -52,10 +59,7 @@ const projectsTech: Project[] = [
                         </a>
                       </div>
                       <div className='ButtonArea Buttons'>
-                        <a href='https://ianvi1.github.io/modalPizza/' target='_blank' rel='noreferrer'>
-                          <FaEye size={28} className='EyeIcon' />
-                          <p>Visualizar</p>
-                        </a>
+                        {pj.deploy}
                       </div>
                     </div>
                   </div>     
