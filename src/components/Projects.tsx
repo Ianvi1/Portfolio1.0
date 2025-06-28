e76c24d24bdbf31567e8ff711d56ce9a9cb3e099
Ian <ianmarques619@gmail.com>
2025-06-28 11:02:01 -0300

    ajustes em projects e adição do projeto 'tirar visto' aos destaque do portfólio

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { VscGithubAlt } from "react-icons/vsc";
 import { FaEye, } from "react-icons/fa";
 import './Projects.scss';
-import project2 from "../../public/imageport2.png";
+import project2 from "../../public/imgdestaque1.png";
 import project3 from "../../public/imageport3.png";
 import project6 from "../../public/imageport6.png";
 
@@ -19,21 +19,21 @@ interface Project {
 } 
 
 const projectsTech: Project[] = [
-  { id: "project1", name: "Project Starbucks", text: "Projeto criado em HTML e CSS pelo curso B7Web, landing page responsivo, clone do starbucks.", img: <img className='TechImage' src={project2} />,techs: "Html e Css.", deploy:<a href='' target='_blank' rel='noreferrer'>
+  { id: "project1", name: "Project TirarVisto", text: "One Page responsiva, utilizando TypeScript e Tailwind junto ao react, com estruturação 'padrão' de pastas e arquivos.", img: <img className='TechImage' src={project2} />,techs: "React, Typescript e Tailwind", deploy:<a href='https://tirar-visto-sepia.vercel.app/' target='_blank' rel='noreferrer'>
   <FaEye size={28} className='EyeIcon' />
   <p>Visualizar</p>
-</a>,githb: <a href='https://github.com/Ianvi1/Starbucks-Layout' target='_blank' rel='noreferrer'>
+</a>,githb: <a href='https://github.com/Ianvi1/tirar-visto' target='_blank' rel='noreferrer'>
   <VscGithubAlt className='GitHubIcon' />
   <p>Github</p>
 </a>},
-  { id: "project2", name: "Project Costs", text: "Projeto em React com api criada, pelo curso React do 'Matheus Battisti'.", img: <img className='TechImage' src={project6} />,techs: "React.", deploy:<a href='https://costs-react-eight.vercel.app/' target='_blank' rel='noreferrer'>
+  { id: "project2", name: "Project Costs", text: "Projeto de serviços completo de React com api criada a mão, com base no curso React do 'Matheus Battisti'.", img: <img className='TechImage' src={project6} />,techs: "React.", deploy:<a href='https://costs-react-eight.vercel.app/' target='_blank' rel='noreferrer'>
   <FaEye size={28} className='EyeIcon' />
   <p>Visualizar</p>
 </a>, githb: <a href='https://github.com/Ianvi1/CostsReact' target='_blank' rel='noreferrer'>
                           <VscGithubAlt className='GitHubIcon' />
                           <p>Github</p>
                         </a>},
-  { id: "project3", name: "Project ModalPizza", text: "Projeto em JavaScript com api criada, modais com pizza, pelo curso B7Web.", img: <img className='TechImage' src={project3} />,techs: "Html, Css e JavaScript.", deploy:<a href='https://ianvi1.github.io/modalPizza/' target='_blank' rel='noreferrer'>
+  { id: "project3", name: "Project ModalPizza", text: "Modal de pizzas básico, utilizando uma Api fornecida e JavaScript, com base no curso B7Web.", img: <img className='TechImage' src={project3} />,techs: "Html, Css e JavaScript.", deploy:<a href='https://ianvi1.github.io/modalPizza/' target='_blank' rel='noreferrer'>
   <FaEye size={28} className='EyeIcon' />
   <p>Visualizar</p>
 </a>, githb: <a href='https://github.com/Ianvi1/modalPizza' target='_blank' rel='noreferrer'>
